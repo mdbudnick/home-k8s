add9bb2f5e06540801e0edbd01b497292ff4ac14
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-12-12 19:21:29 -0500

    Add -t -q and -v arguments to generate-inventory.sh

--- generate-inventory.sh
+++ generate-inventory.sh
@@ -1,27 +1,58 @@
 #!/bin/bash
 
-echo "Running nmap to discover network devices"
+target_file=false
+quiet_mode=false
+verbose=false
+
+while getopts ":qtd:" opt; do
+  case $opt in
+    q)
+      quiet_mode=true
+      ;;
+    v)
+      verbose=true
+      ;;
+    t)
+      target_file="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if $verbose; then
+    echo "Running nmap to discover network devices"
+fi
 nmap_output=$(nmap -sn '10.109.89.*' | grep 'Nmap scan report for' | awk '{print $5}')
 
-echo "Collecting device details with arp - a"
+if $verbose; then
+    echo "Collecting device details with arp - a"
+fi
 arp_output=$(arp -a | grep -v incomplete)
 
 inventory_file="/tmp/k8s_inventory"
 echo "[k8s_cluster:children]" >> "$inventory_file"
-echo "k8s_controlplane_nodes" >> "$inventory_file"
-echo "k8s_worker_nodes" >> "$inventory_file"
+echo "k8s_controlplane_nodes, k8s_worker_nodes" >> "$inventory_file"
 echo "" >> "$inventory_file"
-echo "Initialized a master inventory file"
+if $verbose; then
+    echo "Initialized a master inventory file"
+fi
 
 controlplane_file="/tmp/k8s_controlplanes"
 echo "[k8s_controlplane_nodes]" >> "$controlplane_file"
 controlplane_counter=0
-echo "Initialized a controlplane inventory file"
+if $verbose; then
+    echo "Initialized a controlplane inventory file"
+fi
 
 workernode_file="/tmp/k8s_workernodes"
 echo "[k8s_worker_nodes]" >> "$workernode_file"
 workernode_counter=0
-echo "Initialized worker node inventory file"
+if $verbose; then
+    echo "Initialized worker node inventory file"
+fi
 
 # Process arp_output and generate inventory entries
 while read -r entry; do
@@ -30,13 +61,17 @@ while read -r entry; do
     ip=$(echo "$entry" | awk '{print $2}' | tr -d '()')
 
     if [[ $hostname =~ wyse ]]; then
-        echo "Found $hostname at $ip, adding as controlplane-$controlplane_counter"
+        if $verbose; then
+            echo "Found $hostname at $ip, adding as controlplane-$controlplane_counter"
+        fi
         echo "controlplane-$controlplane_counter ansible_host=$ip" >> "$controlplane_file"
         ((controlplane_counter++))
     fi
 
     if [[ $hostname =~ pi.*k8s ]]; then
-        echo "Found $hostname at $ip, adding as workernode-$workernode_counter"
+        if $verbose; then
+            echo "Found $hostname at $ip, adding as workernode-$workernode_counter"
+        fi
         echo "workernode-$workernode_counter ansible_host=$ip" >> "$workernode_file"
         ((workernode_counter++))
     fi
@@ -47,6 +82,21 @@ echo "" >> "$inventory_file"
 cat "$workernode_file" >> "$inventory_file"
 echo "" >> "$inventory_file"
 
-echo "Generated inventory file:"
-echo ""
-cat "$inventory_file"
+if $verbose; then
+    echo "Generated inventory file:"
+    echo ""
+fi
+if ! $quiet_mode; then
+    cat "$inventory_file"
+fi
+
+if $target_file; then
+    cp "$inventory_file" "$target_file"
+    if $verbose; then
+        echo "Created inventory file at $target_file"
+    fi
+fi
+
+rm "$inventory_file"
+rm "$controlplane_file"
+rm "$workernode_file"
